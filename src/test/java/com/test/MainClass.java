199f6cd40735b36c2f1b539b5718a3ce01af72fb
rajaprpr <rajaprpr123@gmail.com>
2020-09-26 10:38:19 +0530

    Team Two code

--- src/test/java/com/test/MainClass.java
+++ src/test/java/com/test/MainClass.java
@@ -2,18 +2,15 @@ package com.test;
 
 public class MainClass {
 
-	public static void first_Method() {
-
-	}
-
 	public static void second_Method() {
-
+		System.out.println("Second team done");
 	}
+
 	public static void third_Method() {
 
 	}
 
 	public static void main(String[] args) {
-
+		second_Method();
 	}
 }
