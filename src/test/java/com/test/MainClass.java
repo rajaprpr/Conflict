27943e0c0b90b4bcff9c9df10df0a0053a00cf7f
rajaprpr <rajaprpr123@gmail.com>
2020-09-26 10:46:14 +0530

    updated code team two

--- src/test/java/com/test/MainClass.java
+++ src/test/java/com/test/MainClass.java
@@ -5,6 +5,9 @@ public class MainClass {
 	public static void second_Method() {
 		System.out.println("Second team done");
 	}
+	public static void second_Done() {
+		System.out.println("Second team");
+	}
 
 	public static void third_Method() {
 
@@ -12,5 +15,6 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		second_Method();
+		second_Done();
 	}
 }
