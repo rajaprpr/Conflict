aa6b821b0a8670878e9a9add18ae06e83455341e
rajaprpr <rajaprpr123@gmail.com>
2020-09-26 10:22:15 +0530

    master code

---
+++ src/test/java/com/test/MainClass.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class MainClass {
+
+	public static void first_Method() {
+
+	}
+
+	public static void second_Method() {
+
+	}
+	public static void third_Method() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
